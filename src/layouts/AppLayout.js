5ec998e6f8d213a025d92430a2c2bf0b4ee6f97b
AbeethaHeshan <abeethaheshan400@gmail.com>
2024-02-13 22:48:33 +0530

    create the  main  project structure

---
+++ src/layouts/AppLayout.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../components/header/Header'
+import Routers from '../routes/Routers'
+import Footer from '../components/footer/Footer'
+
+export default function AppLayout() {
+  return (
+    <>
+      <Header />
+        <main>
+          <Routers />
+        </main>
+      <Footer />
+    </>
+  )
+}
